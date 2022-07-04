87cf8f75a6e99395ff3724ccc4055b859e549eea
EnzoAvelar <102835008+EnzoAvelar@users.noreply.github.com>
2022-07-04 19:56:16 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,147 +1,112 @@
-//Estados de Jogo
-var PLAY=1;
-var END=0;
-var gameState=1;
-
-var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
-var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
-var gameOverSound ,knifeSwoosh;
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
 
 function preload(){
-  
-  knifeImage = loadImage("knife.png");
-  monsterImage = loadAnimation("alien1.png","alien2.png")
-  fruit1 = loadImage("fruit1.png");
-  fruit2 = loadImage("fruit2.png");
-  fruit3 = loadImage("fruit3.png");
-  fruit4 = loadImage("fruit4.png");
-  gameOverImage = loadImage("fimdeJogo.png")
-  
-  gameOverSound = loadSound("gameover.mp3")
-  knifeSwooshSound = loadSound("knifeSwoosh.mp3")
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
 }
 
-
-
-function setup() {
-  createCanvas(600, 600);
+function setup(){
+  createCanvas(600,600);
+  spookySound.loop();
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
   
-  //criando espada
-   knife=createSprite(40,200,20,20);
-   knife.addImage(knifeImage);
-   knife.scale=0.7
+  doorsGroup = new Group();
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
   
-  
-  
-  //definir colisor para espada
-  knife.setCollider("rectangle",0,0,40,40);
-
-  //Variáveis de pontuação e Grupos
-  score=0;
-  fruitGroup=createGroup();
-  monsterGroup=createGroup();
-  
+  ghost = createSprite(200,200,50,50);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
 }
 
-function draw() {
-  background("lightblue");
-  
-  if(gameState===PLAY){
-    
-    //Chamar função de frutas e função de monstro
-    fruits();
-    Monster();
-    
-    //mover espada com o mouse
-    knife.y=World.mouseY;
-    knife.x=World.mouseX;
-  
-    //Aumenta a pontuação se a espada tocar na fruta
-    if(fruitGroup.isTouching(knife)){
-      fruitGroup.destroyEach();
-      
-       knifeSwooshSound.play();
-      // knifeSwooshSound.play;
-      // knifeSwooshSound();
-      // knifeSwooshSoundplay();
-
-
-      // score=score;
-      // score=+2;
-      // score=2;
-       score=score+2;
-      
+function draw(){
+  background(0);
+  if (gameState === "play") {
+    if(keyDown("left_arrow")){
+      ghost.x = ghost.x - 3;
     }
-    else
-    {
-      //Vá para o estado final se a espada tocar o inimigo
-      if(monsterGroup.isTouching(knife)){
-        gameState=END;
-        //som de fim de jogo/gameover
-        gameOverSound.play()
-        
-        fruitGroup.destroyEach();
-        monsterGroup.destroyEach();
-        fruitGroup.setVelocityXEach(0);
-        monsterGroup.setVelocityXEach(0);
-        
-        //Mude a animação da espada para fim de jogo e redefina sua posição
-        knife.addImage(gameOverImage);
-        knife.scale=1;
-        knife.x=300;
-        knife.y=300;
-      }
+    
+    if(keyDown("right_arrow")){
+      ghost.x = ghost.x + 3;
     }
+    
+    if(keyDown("space")){
+      ghost.velocityY = -10;
+    }
+    
+    ghost.velocityY = ghost.velocityY + 0.8
+    
+    if(tower.y > 400){
+      tower.y = 300
+    }
+    spawnDoors();
+
+    
+    //climbersGroup.collide(ghost);
+    if(climbersGroup.isTouching(ghost)){
+      ghost.velocityY = 0;
+    }
+    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
+      ghost.destroy();
+      gameState = "end"
+    }
+    
+    drawSprites();
   }
   
-  drawSprites();
-  //exibir pontuação
-  textSize(25);
-  text("Pontuação: "+ score,250,50);
-}
-
-
-function Monster(){
-  if(World.frameCount%200===0){
-    monster=createSprite(400,200,20,20);
-    monster.addAnimation("moving", monsterImage);
-    monster.y=Math.round(random(100,550));
-    monster.velocityX=-(8+(score/10));
-    monster.setLifetime=50;
-    
-    monsterGroup.add(monster);
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Fim de Jogo", 230,250)
   }
+
 }
 
-function fruits(){
-  if(World.frameCount%80===0){
-    fruit=createSprite(400,200,20,20);
-    fruit.x = 0    
-  //aumentar a velocidade das frutas após a pontuação 4 
-
-       fruit.velocityX= (7+(score/4));
-      // fruit.velocityY= (7+(score));
-      //  fruit.velocity= (7+(score/4));
-      // fruit.velocityX= (7);
-     
-    fruit.scale=0.2;
-     //fruit.debug=true;
-     r=Math.round(random(1,4));
-    if (r == 1) {
-      fruit.addImage(fruit1);
-    } else if (r == 2) {
-      fruit.addImage(fruit2);
-    } else if (r == 3) {
-      fruit.addImage(fruit3);
-    } else {
-      fruit.addImage(fruit4);
-    }
+function spawnDoors() {
+  //escreva aqui o código para gerar as portas na torre
+  if (frameCount % 240 === 0) {
+    var door = createSprite(200, -50);
+    var climber = createSprite(200,10);
+    var invisibleBlock = createSprite(200,15);
+    invisibleBlock.width = climber.width;
+    invisibleBlock.height = 2;
     
-    fruit.y=Math.round(random(50,550));
+    door.x = Math.round(random(120,400));
+    climber.x = door.x;
+    invisibleBlock.x = door.x;
+    
+    door.addImage(doorImg);
+    climber.addImage(climberImg);
+    
+    door.velocityY = 1;
+    climber.velocityY = 1;
+    invisibleBlock.velocityY = 1;
+    
+    ghost.depth = door.depth;
+    ghost.depth +=1;
    
+    //designe tempo de vida a variável
+    door.lifetime = 800;
+    climber.lifetime = 800;
+    invisibleBlock.lifetime = 800;
+
     
-    fruit.setLifetime=100;
-    
-    fruitGroup.add(fruit);
+    //adicione cada porta ao grupo
+    doorsGroup.add(door);
+    invisibleBlock.debug = true;
+    climbersGroup.add(climber);
+    invisibleBlockGroup.add(invisibleBlock);
   }
 }
+
